0ba000b5175ee4159efc42489c2efa45d9907eb6
ceoservicehp <ceoservicehp@gmail.com>
2026-02-21 15:05:42 +0700

    Update nota.js

--- js/nota.js
+++ js/nota.js
@@ -169,15 +169,16 @@ function downloadPDF(){
 
   const element = document.querySelector(".invoice");
 
-  html2pdf().set({
-    margin:0.5,
-    filename:"Invoice_CEO_"+currentData.id+".pdf",
-    image:{ type:'jpeg', quality:0.98 },
-    html2canvas:{ scale:2 },
-    jsPDF:{ unit:'in', format:'a4', orientation:'portrait' }
-  }).from(element).save();
-}
+  const opt = {
+    margin:       0.3,
+    filename:     "Invoice_CEO_"+currentData.id+".pdf",
+    image:        { type: 'jpeg', quality: 1 },
+    html2canvas:  { scale: 3, useCORS:true },
+    jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
+  };
 
+  html2pdf().set(opt).from(element).save();
+}
 
 /* ================= WHATSAPP ================= */
 function sendWhatsApp(){
