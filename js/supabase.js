8a623cf50d8c435b02bc00aee1fbea14683bccb1
ceoservicehp <ceoservicehp@gmail.com>
2026-02-25 09:55:30 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,6 +2,6 @@ const SUPABASE_URL = "https://aqvcalymmxqwadipbzwg.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxdmNhbHltbXhxd2FkaXBiendnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwODA3NzcsImV4cCI6MjA4NjY1Njc3N30.2pTQWjTD7OeEoA4DXPLqfAVvB6u_DedLVJvyxrTSpHA";
 
 window.supabaseClient = supabase.createClient(
-  supabaseUrl,
-  supabaseKey
+  SUPABASE_URL,
+  SUPABASE_KEY
 );
