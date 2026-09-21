077f91484d864ff706a9066f5494cfe2df7b7683
ceoservicehp <ceoservicehp@gmail.com>
2026-09-21 14:15:17 +0700

    Delete js/config.js

--- js/config.js
+++
@@ -1,5 +0,0 @@
-// Isi dengan Project URL dan anon/public key dari Supabase > Project Settings > API.
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://aqvcalymmxqwadipbzwg.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxdmNhbHltbXhxd2FkaXBiendnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwODA3NzcsImV4cCI6MjA4NjY1Njc3N30.2pTQWjTD7OeEoA4DXPLqfAVvB6u_DedLVJvyxrTSpHA"
-};
